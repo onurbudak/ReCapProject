9aa79d67401dbec2a2f75d0bfaea8847e391c4b5
Onur Budak <onurbudak1997@outlook.com>
2021-02-03 22:41:20 +0300

    2 Şubat 2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,12 +9,16 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            InMemoryMethod();
+        }
 
+        private static void InMemoryMethod()
+        {
             CarManager carManager = new CarManager(new InMemoryCarDal());
 
             foreach (var car in carManager.GetAll())
             {
-                Console.WriteLine("{0} {1} {2} {3} {4} {5}", car.Id, car.BrandId, car.ColorId, car.ModelYear,car.DailyPrice, car.Description);
+                Console.WriteLine("{0} {1} {2} {3} {4} {5}", car.Id, car.BrandId, car.ColorId, car.ModelYear, car.DailyPrice, car.Description);
             }
         }
     }
