6cc33af179e7527d6c2a78a403e5c5859d70fbe9
Onur Budak <onurbudak1997@outlook.com>
2021-02-07 13:52:04 +0300

    7 Şubat 2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -19,7 +19,7 @@ namespace ConsoleUI
         private static void EfCarDalMethod()
         {
             CarManager carManager = new CarManager(new EfCarDal());
-            carManager.Add(new Car { BrandId = 1, ColorId = 2, ModelYear = "2018", DailyPrice = 145, Descriptions = "Dizel" });
+            //carManager.Add(new Car { BrandId = 1, ColorId = 2, ModelYear = "2018", DailyPrice = 145, Descriptions = "Dizel" });
             foreach (var car in carManager.GetAll())
             {
                 Console.WriteLine("{0} {1} {2} {3} {4} {5}", car.CarId, car.BrandId, car.ColorId, car.ModelYear, car.DailyPrice, car.Descriptions);
