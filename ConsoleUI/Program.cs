982030fc14425ba46b8b5b361268779cac510978
Onur Budak <onurbudak1997@outlook.com>
2021-02-08 13:37:37 +0300

    8 Şubat 2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,8 +12,39 @@ namespace ConsoleUI
         {
             //InMemoryMethod();
 
-            EfCarDalMethod();
+            //EfCarDalMethod();
 
+            CarCrudMethods();
+        }
+
+        private static void CarCrudMethods()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4} {5}", car.CarId, car.BrandId, car.ColorId, car.ModelYear, car.DailyPrice, car.Descriptions);
+            }
+            Console.WriteLine("-------------------------------------------------------------------------");
+            var carEntity = carManager.Get(1004);
+            Console.WriteLine("{0} {1} {2} {3} {4} {5}", carEntity.CarId, carEntity.BrandId, carEntity.ColorId, carEntity.ModelYear, carEntity.DailyPrice, carEntity.Descriptions);
+            Console.WriteLine("-------------------------------------------------------------------------");
+            carManager.Add(new Car { BrandId = 1, ColorId = 2, DailyPrice = 238, ModelYear = "2018", Descriptions = "Benzinli" });
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4} {5}", car.CarId, car.BrandId, car.ColorId, car.ModelYear, car.DailyPrice, car.Descriptions);
+            }
+            Console.WriteLine("-------------------------------------------------------------------------");
+            carManager.Update(new Car { CarId = 1004, BrandId = 3, ColorId = 3, DailyPrice = 578, ModelYear = "2009", Descriptions = "Dizel" });
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4} {5}", car.CarId, car.BrandId, car.ColorId, car.ModelYear, car.DailyPrice, car.Descriptions);
+            }
+            Console.WriteLine("-------------------------------------------------------------------------");
+            carManager.Delete(new Car { CarId = 1002, BrandId = 1, ColorId = 2, DailyPrice = 238, ModelYear = "2018", Descriptions = "Benzinli" });
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4} {5}", car.CarId, car.BrandId, car.ColorId, car.ModelYear, car.DailyPrice, car.Descriptions);
+            }
         }
 
         private static void EfCarDalMethod()
