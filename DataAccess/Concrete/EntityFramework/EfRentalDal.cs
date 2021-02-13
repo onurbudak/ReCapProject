0cd61cfdcae68fb8e14e3aacde9d991fb2a6cea7
Onur Budak <onurbudak1997@outlook.com>
2021-02-13 15:47:13 +0300

    13 Şubat 2021

---
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -0,0 +1,15 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Absract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfRentalDal : EfEntityRepositoryBase<Rental,RentACarContext>, IRentalDal
+    {
+      
+    }
+}
---
+++ Entities/Mappers/RentalMapper.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Mappers
+{
+    public class RentalMapper:IEntityTypeConfiguration<Rental>
+    {
+        public void Configure(EntityTypeBuilder<Rental> builder)
+        {
+            builder.Property(r => r.Id);
+           
+        }
+
+       
+    }
+}
