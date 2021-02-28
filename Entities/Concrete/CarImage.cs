c7c581146b30129ed1f24b96abd30733c23b6c90
Onur Budak <onurbudak1997@outlook.com>
2021-02-28 14:50:11 +0300

    28 Şubat 2021

--- Entities/Concrete/CarImage.cs
+++ Entities/Concrete/CarImage.cs
@@ -13,7 +13,7 @@ namespace Entities.Concrete
         }
 
         public int Id { get; set; }
-        public int? CarId { get; set; }
+        public int CarId { get; set; }
         public string ImagePath { get; set; }
         public DateTime? ImageDate { get; set; }
     }
